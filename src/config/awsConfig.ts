7639f0249d4d1953ffd6c4622c07a057f911fc1a
Aayusharma07 <Aayush.Sharma@motherson.com>
2023-06-16 19:34:14 +0530

    get api created

---
+++ src/config/awsConfig.ts
@@ -0,0 +1,9 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+});
+
+export default AWS;
