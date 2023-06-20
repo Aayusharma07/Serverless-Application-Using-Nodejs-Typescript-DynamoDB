c23e48bafd14cd6ffdb29faa698ad0a8cea81c99
Aayusharma07 <Aayush.Sharma@motherson.com>
2023-06-20 15:02:58 +0530

    blog post api created

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,13 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { getAllBlogsController } from './controllers/getAllBlogs.controller';
+import dotenv from 'dotenv';
+import { createBlogController } from './controllers/createBlog.controller';
+dotenv.config();
 
 // Get All Blogs From DynamoDB
 export const getAllBlogs = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> =>
   getAllBlogsController(event, context);
+
+// Create Blog and save to DynamoDB
+export const createBlog = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> =>
+  createBlogController(event, context);
