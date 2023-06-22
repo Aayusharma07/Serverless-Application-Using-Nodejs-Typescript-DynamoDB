877e7f4fd5312634760d10b21a050d3eb9fea02e
Aayusharma07 <Aayush.Sharma@motherson.com>
2023-06-22 11:41:00 +0530

    pagination done on get all blogs api

--- src/controllers/getAllBlogs.controller.ts
+++ src/controllers/getAllBlogs.controller.ts
@@ -3,9 +3,12 @@ import { getAllData } from '../services/dynamodb.service';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getAllBlogsController = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  const { limit, exclusiveStartKey } = event.queryStringParameters || {};
   try {
     const params = {
       TableName: process.env.BLOGS_TABLE || '',
+      Limit: limit ? parseInt(limit, 10) : 10,
+      ExclusiveStartKey: exclusiveStartKey ? JSON.parse(decodeURIComponent(exclusiveStartKey)) : undefined,
     };
     const result = await getAllData(params);
     const response: APIGatewayProxyResult = {
